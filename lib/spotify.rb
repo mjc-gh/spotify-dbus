158d491d7c70b7b922228eb029ffa45b12949480
mikeycgto <mikeycgto@gmail.com>
2011-07-15 15:54:48 -0400

    modify code

---
+++ lib/spotify.rb
@@ -0,0 +1,20 @@
+require 'dbus'
+
+module Spotify
+  class << self
+    def player
+      @@player ||= player_object
+    end
+    
+    private
+
+    def player_object
+      dbus = DBus::SessionBus.instance
+      service = dbus.service('org.mpris.MediaPlayer2.spotify')
+      object = service.object('/org/mpris/MediaPlayer2')
+      object.introspect
+      
+      object['org.mpris.MediaPlayer2.Player']
+    end
+  end
+end
