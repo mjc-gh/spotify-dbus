cf6bfba969914846d45d327ff0185a8d40b65091
mikeycgto <mikeycgto@gmail.com>
2011-08-10 01:24:54 -0400

    clean up module

--- lib/spotify-dbus.rb
+++ lib/spotify-dbus.rb
@@ -1,20 +1,18 @@
 require 'dbus'
 
 module Spotify
-  class << self
-    def player
-      @player ||= player_object
-    end
-    
-    private
-
-    def player_object
-      dbus = DBus::SessionBus.instance
-      service = dbus.service('org.mpris.MediaPlayer2.spotify')
-      object = service.object('/org/mpris/MediaPlayer2')
-      object.introspect
-      
-      object['org.mpris.MediaPlayer2.Player']
-    end
+  def player
+    @player ||= player_object
   end
+    
+  protected
+  
+  def player_object
+    dbus = DBus::SessionBus.instance
+    service = dbus.service('org.mpris.MediaPlayer2.spotify')
+    object = service.object('/org/mpris/MediaPlayer2')
+    object.introspect
+      
+    object['org.mpris.MediaPlayer2.Player']
+  end  
 end
